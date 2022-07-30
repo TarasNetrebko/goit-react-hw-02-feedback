c45d07c1c456095a9b8ef57d16700f55b3566e88
Taras <93833612+TarasNetrebko@users.noreply.github.com>
2022-07-30 16:40:02 +0300

    done3

--- src/components/FeedBack/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedBack/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 
-const Container = styled.div`
+export const Container = styled.div`
   min-width: 215px;
 `
-const Button = styled.button`
+export const Button = styled.button`
   margin-right: 10px;
 `
